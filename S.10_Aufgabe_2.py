3e9697ec80638fa63645aac3511e3024a05297b1
Paul Schworm <paul.schworm@fohren-linden.de>
2022-09-21 10:03:07 +0200

    test

--- S.10_Aufgabe_2.py
+++ S.10_Aufgabe_2.py
@@ -1,8 +1,17 @@
 # This script is the solution for the cm to inch calculator asked for in task 2, from Paul and Max-Leon
+import re
 unit = input("Geben sie das zu konvertierende Maß mitsamt der dazugehörigen Einheit an (Bsp.: 6.8cm / 4.1inch\n")
 unit = unit.lower()
 
-try:
-    if "cm" or "centimeter" or "zentimeter" in unit :
-        unit = float(unit.translate(None, abcdefghijklmnopqrstuvwxyz))
-        converted = round(unit / 2.54,
+if "cm" or "centimeter" or "zentimeter" in unit :
+     unit = float(re.sub('\d, \D', ' ', unit))
+     converted = round(unit / 2.54, 2)
+     prefix = "cm"
+
+elif "i" or "in" or "inch" in unit :
+      unit = re.sub('\d, \D', ' ', unit)
+      unit = float(unit.replace(",", "."))
+      converted = round(unit * 2.54, 2)
+      prefix = "inch"
+
+print("Ihr Maß, umgerechnet in", prefix, ", ist:", unit)
