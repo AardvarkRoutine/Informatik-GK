d736bbd40e7e90e343a37418767ba94a869693fe
Paul Schworm <paul.schworm@fohren-linden.de>
2022-09-21 10:42:06 +0200

    addition of extened bmi Extended_bmi_calculator_(AB)

---
+++ Extended_bmi_calculator_(AB).py
@@ -0,0 +1,22 @@
+try:
+    weight_in_kg = input("Bitte geben sie ihr Körpergewicht in kg ein, statt eines Kommas bitte einen Punkt nutzen: Bsp.: 67.3kg \n")
+
+    size_in_m = input("Bitte geben sie ihre Körpergröße in m ein, statt eines Kommas bitte einen Punkt nutzen: Bsp.: 1.76m \n")
+    weight_in_kg = weight_in_kg.lower()
+    size_in_m = size_in_m.lower()
+
+    if "kg" in weight_in_kg :
+        weight_in_kg = weight_in_kg.strip("kg")
+    if "m" in size_in_m:
+        size_in_m = size_in_m.strip("m")
+
+    size_in_m = float(size_in_m)
+    weight_in_kg = float(weight_in_kg)
+
+    bmi = round(weight_in_kg / (size_in_m**2), 2)
+
+except:
+    print("An error has occurred, please restart!")
+
+else: 
+    print("Ihr BMI lautet:", bmi,", gerundet auf 2 Nachkommastellen")
