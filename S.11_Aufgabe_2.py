11be75b25fb7846912039fd65920f85b351b25ef
Paul Schworm <paul.schworm@fohren-linden.de>
2022-09-21 10:32:37 +0200

    hatred and pain

--- S.11_Aufgabe_2.py
+++ S.11_Aufgabe_2.py
@@ -10,8 +10,8 @@ if d < 0:
     print("Die Lösung der Gleichung ist die leere Menge!")
 
 if d > 0:
-    x1 = float(round(pteil - math.sqrt((p / 2)**2 - q)), 2)
-    x2 = float(round(pteil + math.sqrt((p / 2)**2 - q)), 2)
+    x1 = float(round(pteil - math.sqrt((p / 2)**2 - q), 2))
+    x2 = float(round(pteil + math.sqrt((p / 2)**2 - q), 2))
     print("Die Lösungen der Gleichungen sind: x1=", x1, "x2=", x2)
 
 if d == 0:
