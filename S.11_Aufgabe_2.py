42d8b7bda35bb7a45df54a3c34ae0077d1a9ea42
Paul Schworm <paul.schworm@fohren-linden.de>
2022-09-21 09:54:09 +0200

    Addition of "Seite_11_Aufgabe_2, though it is still faulty

---
+++ S.11_Aufgabe_2.py
@@ -0,0 +1,24 @@
+import math
+p = float(input("Geben sie p ein!\n"))
+q = float(input("Geben sie q ein!\n"))
+
+pteil = - (p / 2)
+
+d = float(((p / 2) ** 2) -q)
+
+if d < 0:
+    print("Die Lösung der Gleichung ist die leere Menge!")
+
+if d > 0:
+    x1 = float(pteil - sqrt((p / 2)**2 -q))
+    x2 = float(pteil + sqrt(((p / 2)**2 -q))
+    print("Die Lösungen der Gleichungen sind: x1=", x1, "x2=", x2)
+
+if d == 0:
+    try:
+        x1 = pteil - sqrt(((p / 2)**2) -q)
+        print("Die Gleichung hat nur eine Lösung:", x1)
+    
+    except:
+        x2 = pteil + sqrt(((p / 2)**2) -q)
+        print("Die Gleichung hat nur eine Lösung:", x2)
