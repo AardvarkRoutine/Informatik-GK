6e97f8cfa73c4266c34e538533f6b4ad7c0338e7
paulcmdhack <52427190+paulcmdhack@users.noreply.github.com>
2023-01-10 23:34:52 +0100

    Created gui_daynight with copilot

--- gui_daynight.py
+++ gui_daynight.py
@@ -1,39 +1,18 @@
 from tkinter import *
-
-textis = "Day"
-textsetpoint = "Night"
-bgis = "yellow"
-bgsetpoint = "blue"
-global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
-
-def ChangeState():
-    global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
-    textis , textsetpoint = textsetpoint, textis
-    bgis, bgsetpoint = bgsetpoint, bgis
-    labelDN.config(text = textis, bg = bgis)
-    button.config(text = textsetpoint, bg = bgsetpoint)
-button.config(command = ChangeState())
-
 tkWindow = Tk()
 tkWindow.title("DayNight")
 tkWindow.geometry('170x125')
-
-
-button = Button(master=tkWindow, text=textsetpoint, bg=bgsetpoint)
-
-labelDN = Label(master = tkWindow, text = textis, bg = bgis, font = ("Arial", 20))
+labelDN = Label(master = tkWindow, text = "Day", bg = "yellow", font = ("Arial", 20))
 labelDN.place(x=5, y=5, width=160, height=80)
-
-button = Button(master=tkWindow, text=textsetpoint, bg=bgsetpoint, command=ChangeState())
-button.place(x=45, y=90, width=50, height=30)
-tkWindow.mainloop()
-
+button = Button(master=tkWindow, text="Night", bg="blue")
+button.place(x=60, y=90, width=50, height=30)
 def ChangeState():
-    global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
-    textis , textsetpoint = textsetpoint, textis
-    bgis, bgsetpoint = bgsetpoint, bgis
-    labelDN.config(text = textis, bg = bgis)
-    button.config(text = textsetpoint, bg = bgsetpoint)
-button.config(command = ChangeState())
-
+    global labelDN, button
+    if labelDN["text"] == "Day":
+        labelDN.config(text = "Night", bg = "blue")
+        button.config(text = "Day", bg = "yellow")
+    else:
+        labelDN.config(text = "Day", bg = "yellow")
+        button.config(text = "Night", bg = "blue")
+button.config(command=ChangeState)
 tkWindow.mainloop()
