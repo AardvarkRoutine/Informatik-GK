5a847ac3611cd600e7f16ba679b28fa4bcd5c626
AardvarkRoutine <52427190+paulcmdhack@users.noreply.github.com>
2023-01-10 14:18:23 +0100

    still not working, will debug when at home

--- gui_daynight.py
+++ gui_daynight.py
@@ -1,20 +1,39 @@
 from tkinter import *
+
 textis = "Day"
 textsetpoint = "Night"
 bgis = "yellow"
 bgsetpoint = "blue"
+global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
 
 def ChangeState():
-    global textis, textsetpoint, bgis, bgsetpoint
+    global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
     textis , textsetpoint = textsetpoint, textis
     bgis, bgsetpoint = bgsetpoint, bgis
-    # labelZahl.config(text=str(stand))
-tkWindow = Tk()
-tWindow.title("DayNight")
-tkWindow.geometry('170x125')
-labelDN = Label(master = tkfenster, text = "Day", bg = "yellow", font = ("Arial", 20))
-labelDN.place(x=5, y=5, width=160, height=80)
-button = Button(master=tkFenster, text="change", bg="blue", command=buttonNullClick)
-button.place(x=45, y=90, width=50, height=30)
+    labelDN.config(text = textis, bg = bgis)
+    button.config(text = textsetpoint, bg = bgsetpoint)
+button.config(command = ChangeState())
 
-# tkWindow.mainloop()
+tkWindow = Tk()
+tkWindow.title("DayNight")
+tkWindow.geometry('170x125')
+
+
+button = Button(master=tkWindow, text=textsetpoint, bg=bgsetpoint)
+
+labelDN = Label(master = tkWindow, text = textis, bg = bgis, font = ("Arial", 20))
+labelDN.place(x=5, y=5, width=160, height=80)
+
+button = Button(master=tkWindow, text=textsetpoint, bg=bgsetpoint, command=ChangeState())
+button.place(x=45, y=90, width=50, height=30)
+tkWindow.mainloop()
+
+def ChangeState():
+    global textis, textsetpoint, bgis, bgsetpoint, labelDN, button
+    textis , textsetpoint = textsetpoint, textis
+    bgis, bgsetpoint = bgsetpoint, bgis
+    labelDN.config(text = textis, bg = bgis)
+    button.config(text = textsetpoint, bg = bgsetpoint)
+button.config(command = ChangeState())
+
+tkWindow.mainloop()
