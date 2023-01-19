55a90b3319b2b28cf9686bbb51cb7aaca02becd8
AardvarkRoutine <52427190+AardvarkRoutine@users.noreply.github.com>
2023-01-19 10:20:21 +0100

    i have severe depression now

--- gui_simpleCalc_imp.py
+++ gui_simpleCalc_imp.py
@@ -5,14 +5,14 @@ tkw = Tk()
 tkw.title('Simple Calculator')
 tkw.geometry('500x230')
 
-entry1 = Entry(master=tkw, text = "0", bg='lightgray', width='135')
+entry1 = Entry(master=tkw, text = "", bg='lightgray')
 entry1.place(x=10, y=10, width="135")
-entry2 = Entry(master=tkw, text = "2", bg='lightgray', width='135')
+entry2 = Entry(master=tkw, text = "", bg='lightgray')
 entry2.place(x=155, y=10, width="135")
 def add():
     try:
-        num1 = entry1.get()
-        num2 = entry2.get()
+        num1 = entry1.cget("text")
+        num2 = entry2.cget()
         num1 = float(num1)
         num2 = float(num2)
         ans = num1 + num2
@@ -56,32 +56,29 @@ def div():
 def sqrt():
     try:
         num1 = entry1.get()
-        num2 = entry2.get()
         num1 = float(num1)
         ans = m.sqrt(num1)
         return ans
     except:
-        if num2 >=0:
+        if num1 <=0:
             return "sqrtError"
         else:
             return "Error"
 
 def pot2():
     try:
-        num1 = entry1.get()
+        '''num1 = entry1.get()
         num2 = entry2.get()
         num1 = float(num1)
         num2 = float(num2)
         ans = num1 ** num2
-        return ans
+        return ans '''
+        pass
     except:
         return "Error"
 
-def foo():
-    pass
-
 global clicked
-clicked = foo()
+clicked = None
 
 def click(func):
     global clicked
@@ -89,9 +86,8 @@ def click(func):
 
 def sol():
     global clicked
-    solution = clicked()
-    ansHistory.config(text=str(solution)
-    ansHistory.config(text="\n")
+    solution = click()
+    ansHistory.configure(text=str(solution) + "\n")
 
 ansHistory = Label(master=tkw, bg='lightgray', text='History')
 ansHistory.place(x=300, y=0, width=200, height=300)
@@ -106,7 +102,7 @@ buttonDiv = Button(master=tkw, bg='#FFCFC9', text='/', command=click(div))
 buttonDiv.place(x=227.5, y=50, width=62.5, height=50)
 buttonSqrt = Button(master=tkw, bg='#FFCFC9', text='2. Wurzel', command=click(sqrt))
 buttonSqrt.place(x=10, y=110, width=135, height=50)
-buttonPot2 = Button(master=tkw, bg='#FFCFC9', text='^2', width='180', height = '40', command =click(pot2))
+buttonPot2 = Button(master=tkw, bg='#FFCFC9', text='^2', width='180', height = '40', command=click(pot2))
 buttonPot2.place(x=155, y=110, width=135, height=50)
 buttonAns = Button(master=tkw, bg='#FFCFC9', text='Ans', command=sol)
 buttonAns.place(x=10, y=170, width=280, height=50)
